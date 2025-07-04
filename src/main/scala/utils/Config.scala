cf66f393d2f7407ff07a24d5bef96fca0a6367fa
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-07-05 00:18:28 +0200

    Update Config.scala

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -1,12 +1,12 @@
 package utils
 
 object Config {
-
   // The local directory containing this repository
   val projectDir :String = "<project_dir>"
   // The name of your bucket on AWS S3
   val s3bucketName :String = "<bucket_name>"
-  // The path to the credentials file for AWS (if you follow instructions, this should not be updated)
-  val credentialsPath :String = "/aws_credentials.txt"
-
+  // The path to the credentials file for AWS
+  val credentialsPath :String = projectDir +  "/src/main/res/aws_credentials.txt"
+  // Path to the dataset of itineraries
+  val path_dataset_itineraries :String = projectDir + "/datasets/itineraries.csv"
 }
