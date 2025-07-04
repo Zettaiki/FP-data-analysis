5bb31a41abb04cb56abb7c521028fe2e23a48096
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-07-05 00:20:21 +0200

    Update Config.scala

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -7,6 +7,8 @@ object Config {
   val s3bucketName :String = "<bucket_name>"
   // The path to the credentials file for AWS
   val credentialsPath :String = projectDir +  "/src/main/res/aws_credentials.txt"
+  // Path to the datasets directory
+  val path_datasets :String = projectDir + "/datasets"
   // Path to the dataset of itineraries
-  val path_dataset_itineraries :String = projectDir + "/datasets/itineraries.csv"
+  val path_dataset_itineraries :String = path_datasets + "/itineraries.csv"
 }
